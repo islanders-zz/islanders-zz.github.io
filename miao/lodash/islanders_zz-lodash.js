7e281d47976d711d0be7bdadb88b3835bea0833a
zhaoyong <zhaoyong.tju@qq.com>
2021-07-12 15:58:15 +0800

    chunk

---
+++ miao/lodash/islanders_zz-lodash.js
@@ -0,0 +1,17 @@
+var islanders_zz = function () {
+  
+  function chunk(array, size = 1) {
+    let len = Math.ceil(array.length / size)
+    let res = new Array()
+    let k = 0
+    for (let i = 0; i < len; i++) {
+      res[i] = new Array(size)
+      for (let j = 0; j < size; j++) res[i][j] = array[k++]
+    }
+    return res
+  }
+
+  return {
+    chunk: chunk
+  }
+} ()
