eaa12e2fa9acb1eaa156c8480defb5679db29299
zhaoyong <zhaoyong.tju@qq.com>
2021-07-27 22:41:25 +0800

    22222222222222225

--- miao/lodash/islanders_zz-lodash.js
+++ miao/lodash/islanders_zz-lodash.js
@@ -228,13 +228,13 @@ var islanders_zz = function () {
   }
 
   function lowerFirst(string = '') {
-    if (string.length) return string
+    if (string.length == 0) return string
     let res = string[0].toUpperCase() + string.slice(1)
     return res
   }
 
   function capitalize(string = '') {
-    if (string.length) return string
+    if (string.length == 0) return string
     let res = string[0].toUpperCase() + string.slice(1).toLowerCase()
     return res
   }
